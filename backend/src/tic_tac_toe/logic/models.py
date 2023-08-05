d859cc124989790492a9afe9d4de9fa4ecd9f975
Prithvij2004 <manikandan2200j@gmail.com>
2023-08-05 09:17:04 +0530

    Added Grid and GameState

--- backend/src/tic_tac_toe/logic/models.py
+++ backend/src/tic_tac_toe/logic/models.py
@@ -7,6 +7,20 @@ from typing import Self
 from dataclasses import dataclass
 from functools import cached_property
 
+from tic_tac_toe.logic.validatiors import validate_grid
+
+
+WINNING_PATTERNS = (
+    "???......",
+    "...???...",
+    "......???",
+    "?...?...?",
+    "..?.?.?..",
+    "?..?..?..",
+    ".?..?..?.",
+    "..?..?..?",
+)
+
 
 class Mark(enum.StrEnum):
     CROSS = "X"
@@ -21,9 +35,8 @@ class Mark(enum.StrEnum):
 class Grid:
     cells: str = " "*9
 
-    def __post_init__(self):
-        if not re.match(r"^[ OX]{9}", self.cells):
-            raise ValueError("Must contain 9 cells of: X, O or space")
+    def __post_init__(self) -> None:
+        validate_grid(self)
         
     @cached_property
     def x_count(self) -> int:
@@ -36,3 +49,43 @@ class Grid:
     @cached_property
     def space_count(self) -> int:
         return self.cells.count(' ')
+    
+@dataclass(frozen=True)
+class Move:
+    mark: Mark
+    cell_index: int
+    before_state: "GameState"
+    after_state: "GameState"
+
+@dataclass(frozen=True)
+class GameState:
+    grid: Grid
+    starting_mark: Mark = Mark("X")
+
+    @cached_property
+    def current_mark(self) -> Mark:
+        return self.starting_mark if self.grid.x_count == self.grid.o_count \
+        else self.starting_mark.other
+    
+    @cached_property
+    def game_not_started(self) -> bool:
+        return self.grid.space_count == 9
+    
+    @cached_property
+    def game_pver(self) -> bool:
+        return self.winner is not None or self.tie
+    
+    @cached_property
+    def tie(self) -> bool:
+        return self.winner is None and self.space_count == 0
+    
+    @cached_property
+    def winner(self) -> list[int]:
+        for pattern in WINNING_PATTERNS:
+            for mark in Mark:
+                if re.match(pattern.replace('?', mark), self.grid.cells):
+                    return [
+                        match.start()
+                        for match in re.finditer(r"\?", pattern)
+                    ]
+        return []
